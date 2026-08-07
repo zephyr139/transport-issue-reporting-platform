992f98a6644493033f374d77ff9fc06b23b9522e
Oleksandr Rybalko <oleksandr.rybalko@student.tuke.sk>
2026-08-07 13:09:56 +0200

    project set up (excluding frontend components)

---
+++ backend/src/main/java/com/zephyr_jarvis/transport_issue_reporting_platform/TransportIssueReportingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zephyr_jarvis.transport_issue_reporting_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportIssueReportingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportIssueReportingPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zephyr_jarvis/transport_issue_reporting_platform/TransportIssueReportingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zephyr_jarvis.transport_issue_reporting_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportIssueReportingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
